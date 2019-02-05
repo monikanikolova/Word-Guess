333c9e7a4ab940a7b6663b8508acea353ea00444
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-02-05 00:41:32 -0500

    Not working yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ var number = Math.floor(Math.random()*4);
 var answer = words[number];
 var myLength = answer.length;
 var display = [myLength];
+var win = myLength; 
 var letters = answer.split('');
 var triesLeft = 10;
 var result = "";
@@ -16,7 +17,7 @@ var userLetters = "";
 
 
 console.log(answer);
-
+//Set up function
 var setup = function() {
     for (i = 0; i < answer.length; i++){
         display[i] = "_ ";
@@ -25,12 +26,40 @@ var setup = function() {
     document.getElementById("game").innerHTML = result;
     result = "";
 }
-
+//Execute it
 window.onload = function(){
     setup();
-}
+} 
 
 var play = function() {
     result = "";
-    userLetters = document.getElementById("letters").value;
+    document.onkeyup=userLetters;
+    console.log(userLetters);
+    
+
+    for (var b = 0; b < answer.length; b++){
+        console.log(letters[b]);
+        if (userLetters.toLocaleUpperCase() === letters[b]) {
+            display[b] = userLetters.toUpperCase();
+            win--;
+        }
+        result = result + display[b] + " ";
+        }
+        document.getElementById("game").innerHTML = result;
+        result = "";
+        triesLeft--;
+        if (win < 1) {
+            document.getElementById("guesses").innerHTML = "You Win!";
+        }
+        else if (triesLeft < 1) {
+            document.getElementById("guesses"),innerHTML = "You lose!";
+
+        }
+        else {
+            document.getElementById("guesses").innerHTML = "You have " + triesLeft + " guesses left";
+        }
+
+}
+window.onload = function () {
+    play();
 }
