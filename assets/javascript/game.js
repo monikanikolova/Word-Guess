088df7eaad0cb38c548893e07a0ba13707cb5239
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-02-05 16:08:04 -0500

    Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 var doc = function (id) {
-   return document.getElementById(id); 
+    return document.getElementById(id);
 }
 
 //Declare variables
 var words = ["SCORPIONS", "DEEP PURPLE", "PINK FLOYD", "BLACK SABBATH"];
-var number = Math.floor(Math.random()*4);
+var number = Math.floor(Math.random() * words.length);
 var answer = words[number];
 var myLength = answer.length;
-var display = [myLength];
-var win = myLength; 
+var display = "";
+var win = myLength;
 var letters = answer.split('');
 var triesLeft = 10;
 var result = "";
@@ -18,32 +18,37 @@ var userLetters = "";
 
 console.log(answer);
 //Set up function
-var setup = function() {
-    for (i = 0; i < answer.length; i++){
-        display[i] = "_ ";
-        result = result + display;
+var setup = function () {
+    for (i = 0; i < myLength; i++) {
+        if (answer[i] !== " ") {
+            display += "_ ";
+        } else {
+            display += " ";
+        }
+
+        result = display;
     }
+
     document.getElementById("game").innerHTML = result;
-    result = "";
 }
 //Execute it
-window.onload = function(){
+window.onload = function () {
     setup();
-} 
+}
 
-var play = function() {
-    result = "";
-    document.onkeyup=userLetters;
-    console.log(userLetters);
-    
 
-    for (var b = 0; b < answer.length; b++){
-        console.log(letters[b]);
-        if (userLetters.toLocaleUpperCase() === letters[b]) {
-            display[b] = userLetters.toUpperCase();
-            win--;
-        }
-        result = result + display[b] + " ";
+document.onkeyup = function (event) {
+        var userLetters = event.key.toUpperCase;
+        console.log(userLetters);
+
+
+        for (var i = 0; i < answer.length; i++) {
+            console.log(letters[i]);
+            if (userLetters === letters[i]) {
+                display[i] = userLetters;
+                win--;
+            }
+            result = display[i] + " ";
         }
         document.getElementById("game").innerHTML = result;
         result = "";
@@ -52,14 +57,14 @@ var play = function() {
             document.getElementById("guesses").innerHTML = "You Win!";
         }
         else if (triesLeft < 1) {
-            document.getElementById("guesses"),innerHTML = "You lose!";
+            document.getElementById("guesses"), innerHTML = "You lose!";
 
         }
         else {
             document.getElementById("guesses").innerHTML = "You have " + triesLeft + " guesses left";
         }
 
-}
-window.onload = function () {
-    play();
-}
+    }
+    
+    
+
