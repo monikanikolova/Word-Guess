575f8c6ca9fbbc35327c031856e0c55644fbd16b
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-02-06 20:31:50 -0500

    Working, no style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,72 @@
-var doc = function (id) {
-    return document.getElementById(id);
-}
-
 //Declare variables
 var words = ["SCORPIONS", "DEEP PURPLE", "PINK FLOYD", "BLACK SABBATH"];
 var number = Math.floor(Math.random() * words.length);
 var answer = words[number];
 var myLength = answer.length;
-var display = "";
+var display = [];
 var win = myLength;
 var letters = answer.split('');
 var triesLeft = 10;
 var result = "";
 var userLetters = "";
+var guessedLetters = [];
+var gameOver = false;
 
-
-
-console.log(answer);
 //Set up function
 var setup = function () {
-    for (i = 0; i < myLength; i++) {
-        if (answer[i] !== " ") {
-            display += "_ ";
-        } else {
-            display += " ";
-        }
-
-        result = display;
+  for (i = 0; i < myLength; i++) {
+    if (answer[i] !== " ") {
+      display.push("_");
+    } else {
+      display.push(" ");
+      win--;
     }
 
-    document.getElementById("game").innerHTML = result;
+    result = display;
+  }
+
+  displayOnDom(result);
 }
 //Execute it
 window.onload = function () {
-    setup();
+  setup();
+}
+
+function displayOnDom(thingToDisplay) {
+  document.getElementById("game").innerHTML = thingToDisplay.join(' ');
+}
+
+document.onkeyup = function (event) {
+  if (!gameOver) {
+
+    var userLetters = event.key.toUpperCase();
+
+
+    if (guessedLetters.indexOf(userLetters) === -1 && letters.indexOf(userLetters) !== -1) {
+      for (var i = 0; i < answer.length; i++) {
+        if (userLetters === letters[i]) {
+          display[i] = userLetters;
+          displayOnDom(display);
+          win--;
+          guessedLetters.push(userLetters);
+
+        }
+      }
+    } else {
+      triesLeft--;
+    }
+    if (win < 1) {
+      document.getElementById("guesses").innerHTML = "You Win!";
+      gameOver = true;
+    } else if (triesLeft < 1) {
+      document.getElementById("guesses").innerHTML = "You lose!";
+      gameOver = true;
+    } else {
+      document.getElementById("guesses").innerHTML = "You have " + triesLeft + " guesses left";
+    }
+
+  }
 }
 
 
-document.onkeyup = function (event) {
-        var userLetters = event.key.toUpperCase;
-        console.log(userLetters);
-
-
-        for (var i = 0; i < answer.length; i++) {
-            console.log(letters[i]);
-            if (userLetters === letters[i]) {
-                display[i] = userLetters;
-                win--;
-            }
-            result = display[i] + " ";
-        }
-        document.getElementById("game").innerHTML = result;
-        result = "";
-        triesLeft--;
-        if (win < 1) {
-            document.getElementById("guesses").innerHTML = "You Win!";
-        }
-        else if (triesLeft < 1) {
-            document.getElementById("guesses"), innerHTML = "You lose!";
-
-        }
-        else {
-            document.getElementById("guesses").innerHTML = "You have " + triesLeft + " guesses left";
-        }
-
-    }
-    
-    
 
